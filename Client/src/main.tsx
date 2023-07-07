81bf10fe19aa89f1cb60f7470c6e7ce148f8f927
remijarry <rem_ay@hotmail.fr>
2023-07-07 13:13:51 +1200

    Starting from scratch

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,30 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { BrowserRouter } from 'react-router-dom';
-
-import './index.css';
-import { AppProvider } from './contexts/appContext';
-import { Auth0Provider } from '@auth0/auth0-react';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Auth0Provider
-                domain='makeitcount.au.auth0.com'
-                clientId='skIn9wSotf0i0s8yjhnWXWHl8RhbeTsq'
-                authorizationParams={{
-                    redirect_uri: window.location.origin,
-                }}
-            >
-                <AppProvider>
-                    <App />
-                </AppProvider>
-
-            </Auth0Provider>
-        </BrowserRouter>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
